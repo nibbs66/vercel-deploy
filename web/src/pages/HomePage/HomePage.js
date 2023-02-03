c3f124ac09ffff73a0947ba1ff90a3a8a17ceea5
Chris <75439614+chris-mcnabb@users.noreply.github.com>
2023-02-03 13:06:16 +0100

    Initial commit

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+
+      <h1>HomePage</h1>
+
+    </>
+  )
+}
+
+export default HomePage
